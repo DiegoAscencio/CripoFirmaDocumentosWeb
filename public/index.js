71cae089607cd564c5178edcf2e74e4bcfe9f87a
DiegoAscencio <is698355@iteso.mx>
2020-11-25 21:07:51 -0600

    Shorter code

--- public/index.js
+++ public/index.js
@@ -1,35 +1,26 @@
-let list_element_in_html = document.getElementById('files');
+fetch('/Files')
+    .then(response => response.json())
+    .then(data => displayFiles(data.files));
 
-    fetch('/Files')
-      .then(response => response.json())
-      .then(data => displayFiles(data.files));
-
-
-    function displayFiles(files) {
-      for (file of files) {
-        let tr = document.createElement("tr");
-        tr.appendChild(document.createTextNode(file));
-        list_element_in_html.appendChild(tr);
-      }
+function displayFiles(files) {
+    for (file of files) {
+        $("#files").append(`<div>${file}<a href="https://www.google.com/">Download</a></div>`);
     }
+}
 
-    let list_element_in_html_signed = document.getElementById('singedfiles');
+fetch('/signedFiles')
+    .then(response => response.json())
+    .then(data => displaySignedFiles(data.signedfiles));
 
-    fetch('/signedFiles')
-      .then(response => response.json())
-      .then(data => displaySignedFiles(data.signedfiles));
-
-    function displaySignedFiles(files) {
-      console.log(files);
-      for (file of files) {
-        let tr = document.createElement("tr");
-        tr.appendChild(document.createTextNode(file));
-        list_element_in_html_signed.appendChild(tr);
-      }
+function displaySignedFiles(files) {
+    console.log(files);
+    for (file of files) {
+        $("#singedfiles").append(`<div>${file}<a href="https://www.google.com/">Download</a></div>`);
     }
+}
 
-    function verify() {
-      fetch('/verifyFiles')
+function verify() {
+    fetch('/verifyFiles')
         .then(response => response.json())
         .then(data => alert(data.message));
-    }
+}
