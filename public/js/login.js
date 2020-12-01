2c85e01b19fd02866893aeaf3eaa5cf598d933ef
DiegoAscencio <is698355@iteso.mx>
2020-12-01 12:00:50 -0600

    Login Log

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,3 @@
-
 localStorage.sessionId;
 localStorage.sessionEmail;
 localStorage.userId;
@@ -19,9 +18,9 @@ function logIn() {
     xhr.open('POST', endpoint);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
-        "email":data.email.value,
-        "password":data.password.value
-      }));
+        "email": data.email.value,
+        "password": data.password.value
+    }));
     xhr.onload = () => {
         if (xhr.status == 200) {
             let user = JSON.parse(xhr.response);
@@ -29,7 +28,30 @@ function logIn() {
             alert(`Bienvenido ${data.email.value}`);
             localStorage.sessionId = "TOKEN";
             localStorage.sessionEmail = user.email;
-            window.location.href="index.html";
+
+            if (xhr.status == 200) {
+                endpoint = `https://localhost:3000/api/logs/`
+
+                xhr.open('POST', endpoint);
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.send(JSON.stringify({
+                    "name": 'name',
+                    "email": data.email.value,
+                    "type": 'LOGIN',
+                }));
+                xhr.onload = () => {
+                    if (xhr.status == 200) {
+                        console.log("Created Log");
+                        window.location.href = "index.html";
+                    } else if (xhr.status == 404) {
+                        console.log("Error Log");
+                    }
+                }
+
+            } else if (xhr.status == 404) {
+                alert("Error al registrar usuario");
+            }
+
         } else if (xhr.status == 404) {
             alert("Usuario o contraseña incorrectos");
         }
@@ -38,5 +60,5 @@ function logIn() {
 
 //CLIC REGISTER
 function register() {
-    window.location.href="register.html";
+    window.location.href = "register.html";
 }
